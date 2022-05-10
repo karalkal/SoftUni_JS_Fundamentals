6606cd8a177946ce307ec28661a810cd0d18c5c2
karalkal <karalkal@yahoo.co.uk>
2022-05-10 19:59:39 +0100

    added solutions to lab_1 tasks 7, 8

---
+++ 01_BasicSyntaxConditionalStatementsLoops/07_count_to_5.js
@@ -0,0 +1,8 @@
+function countTo5() {
+    let i = 1
+    for (i; i < 6; i++) {
+        console.log(i)
+    }
+}
+
+countTo5()
---
+++ 01_BasicSyntaxConditionalStatementsLoops/08_divisible_by_10.js
@@ -0,0 +1,9 @@
+function print_divisible_by_3() {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 == 0) {
+            console.log(i)
+        }
+    }
+}
+
+print_divisible_by_3()
